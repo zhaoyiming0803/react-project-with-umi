0ff56620cb0ef04089d3d568335b7bd372c2b171
zymfe <1047832475@qq.com>
2019-12-26 21:13:08 +0800

    antd-demo

---
+++ antd-demo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Switch } from 'antd';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+    <Switch></Switch>
+  </div>
+);
+
+export default App;
